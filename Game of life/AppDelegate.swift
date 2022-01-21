6efba8630c66f162dbfcea011e64e1a212b0a2ed
jez <jez@wain.name>
2022-01-21 14:20:56 -0600

    Initial Commit

---
+++ Game of life/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Game of life
+//
+//  Created by jez wain on 1/21/22.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
